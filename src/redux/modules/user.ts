44500abaa0525fdc05fe808c5021a584a7f51ce5
joeylam <joey.cm.lam@gmail.com>
2020-08-09 16:43:51 +0800

    add user.ts

--- src/redux/modules/user.ts
+++ src/redux/modules/user.ts
@@ -1,4 +1,13 @@
-export {}
+export function typedAction<T extends string>(type: T): { type: T };
+export function typedAction<T extends string, P extends any>(
+    type: T,
+    payload: P
+): { type: T; payload: P };
+
+export function typedAction(type: string, payload?: any) {
+    return { type, payload };
+}
+
 
 type UserState = {
     username: string | null
@@ -7,15 +16,28 @@ type UserState = {
 const initialState: UserState = { username: null };
 
 
-const login = (username: string) => (
-    {
-        payload: username
-    }
-);
+export const login = (username: string) => {
+    return typedAction('user/LOGIN', username);
+};
 
-const logout = () => (
-    {
+export const logout = () => {
+    return typedAction( 'user/LOGOUT');
+};
 
+type UserAction = ReturnType<typeof login | typeof logout>;
+
+
+export function userReducer(
+    state = initialState,
+    action: UserAction
+): UserState {
+    switch (action.type) {
+        case 'user/LOGIN':
+            return { username: action.payload };
+        case 'user/LOGOUT':
+            return { username: null };
+        default:
+            return state;
     }
-);
+}
 
